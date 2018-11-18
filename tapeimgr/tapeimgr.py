ae1188c9e559a37651286dc28899ecd2e834e702
bitsgalore <johan.vanderknijff@kb.nl>
2018-11-18 17:53:24 +0100

    improved GUI, added fetching and validation of input

--- tapeimgr/tapeimgr.py
+++ tapeimgr/tapeimgr.py
@@ -18,11 +18,14 @@ try:
     from tkinter import filedialog as tkFileDialog
     from tkinter import scrolledtext as ScrolledText
     from tkinter import messagebox as tkMessageBox
+    from tkinter import ttk
 except ImportError:
     import Tkinter as tk  # Python 2.x
     import tkFileDialog
     import ScrolledText
     import tkMessageBox
+    import ttk
+from . import shared
 from . import worker
 from . import config
 
@@ -30,7 +33,7 @@ from . import config
 __version__ = '0.11.0'
 
 
-class carrierEntry(tk.Frame):
+class tapeimgrGUI(tk.Frame):
 
     """This class defines the graphical user interface + associated functions
     for associated actions
@@ -41,7 +44,7 @@ class carrierEntry(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.root = parent
         config.readyToStart = False
-        config.finishedBatch = False
+        config.finishedTape = False
         self.catidOld = ""
         self.titleOld = ""
         self.volumeNoOld = ""
@@ -64,116 +67,41 @@ class carrierEntry(tk.Frame):
             tkMessageBox.showinfo("Exit", msg)
             os._exit(0)
 
-    def on_create(self, event=None):
-        """Create new batch in rootDir"""
-
-        # Set up logging
-        self.setupLogging(self.text_handler)
-
-        # Update state of buttons
-        self.bNew.config(state='disabled')
-        self.bOpen.config(state='disabled')
-        self.bFinalise.config(state='normal')
-        self.submit_button.config(state='normal')
-        config.readyToStart = True
-
     def on_submit(self, event=None):
-        """Process one record and add it to the queue after user pressed submit button"""
+        """fetch and validate entered input"""
 
-        # Fetch entered values (strip any leading / tralue whitespace characters)
-        if config.enablePPNLookup:
-            catid = self.catid_entry.get().strip()
-            self.catidOld = catid
-        else:
-            catid = ""
-            title = self.title_entry.get().strip()
-            self.titleOld = title
-        volumeNo = self.volumeNo_entry.get().strip()
-        self.volumeNoOld = volumeNo
-        carrierTypeCode = self.v.get()
+        # Fetch entered values (strip any leading / traling whitespace characters)
+        config.tapeDevice = self.tapeDevice_entry.get().strip()
+        config.initBlocksize = self.initBlocksize_entry.get().strip()
+        config.prefix = self.prefix_entry.get().strip()
+        config.extension = self.extension_entry.get().strip()
+        config.fillBlocks = self.fBlocks.get()
 
-        # Lookup carrierType for carrierTypeCode value
-        for i in self.carrierTypes:
-            if i[1] == carrierTypeCode:
-                carrierType = i[0]
+        print(config.tapeDevice, config.initBlocksize, config.prefix, config.extension, config.fillBlocks)
 
-        if config.enablePPNLookup:
-            # Lookup catalog identifier
-            sruSearchString = '"PPN=' + str(catid) + '"'
-            response = sru.search(sruSearchString, "GGC")
+        # Check if block size is valid (i.e. a multiple of 512)
+        blocksizeValid = False
+        try:
+            noBlocks = (int(config.initBlocksize)/512)
 
-            if not response:
-                noGGCRecords = 0
+            if not noBlocks.is_integer():
+                msg = "Initial block size must be a multiple of 512"
+                tkMessageBox.showerror("ERROR", msg)
+            elif noBlocks == 0:
+                msg = "Initial block size cannot be 0"
+                tkMessageBox.showerror("ERROR", msg)
             else:
-                noGGCRecords = response.sru.nr_of_records
-        else:
-            noGGCRecords = 1
+                blocksizeValid = True
 
-        if not config.readyToStart:
-            msg = "You must first create a batch or open an existing batch"
-            tkMessageBox.showerror("Not ready", msg)
-        elif not representsInt(volumeNo):
-            msg = "Volume number must be integer value"
-            tkMessageBox.showerror("Type mismatch", msg)
-        elif int(volumeNo) < 1:
-            msg = "Volume number must be greater than or equal to 1"
-            tkMessageBox.showerror("Value error", msg)
-        elif noGGCRecords == 0:
-            # No matching record found
-            msg = ("Search for PPN=" + str(catid) + " returned " +
-                   "no matching record in catalog!")
-            tkMessageBox.showerror("PPN not found", msg)
-        else:
-            if config.enablePPNLookup:
-                # Matching record found. Display title and ask for confirmation
-                record = next(response.records)
-
-                # Title can be in either title element OR in title element with maintitle attribute
-                titlesMain = record.titlesMain
-                titles = record.titles
-
-                if titlesMain != []:
-                    title = titlesMain[0]
-                else:
-                    title = titles[0]
-
-            msg = "Found title:\n\n'" + title + "'.\n\n Is this correct?"
-            if tkMessageBox.askyesno("Confirm", msg):
-                # Prompt operator to insert carrier in disc robot
-                msg = ("Please load disc ('" + title + "', volume " + str(volumeNo) +
-                       ") into the disc loader, then press 'OK'")
-                tkMessageBox.showinfo("Load disc", msg)
-
-                # Create unique identifier for this job (UUID, based on host ID and current time)
-                jobID = str(uuid.uuid1())
-                # Create and populate Job file
-                jobFile = os.path.join(config.jobsFolder, jobID + ".txt")
-
-                if sys.version.startswith('3'):
-                    # Py3: csv.reader expects file opened in text mode
-                    fJob = open(jobFile, "w", encoding="utf-8")
-                elif sys.version.startswith('2'):
-                    # Py2: csv.reader expects file opened in binary mode
-                    fJob = open(jobFile, "wb")
-
-                # Create CSV writer object
-                jobCSV = csv.writer(fJob, lineterminator='\n')
-
-                # Row items to list
-                rowItems = ([jobID, catid, title, volumeNo, carrierType])
-
-                # Write row to job and close file
-                jobCSV.writerow(rowItems)
-                fJob.close()
-
-                # Reset entry fields and set focus on PPN / Title field
-                if config.enablePPNLookup:
-                    self.catid_entry.delete(0, tk.END)
-                    self.catid_entry.focus_set()
-                else:
-                    self.title_entry.delete(0, tk.END)
-                    self.title_entry.focus_set()
-                self.volumeNo_entry.delete(0, tk.END)
+        except ValueError:
+            msg = "Initial block size must be a number"
+            tkMessageBox.showerror("ERROR", msg)
+        
+        if blocksizeValid:
+            # This flag tells worker module tape extraction can start 
+            config.readyToStart = True
+        
+        print(config.readyToStart)
 
     def setupLogging(self, handler):
         """Set up logging-related settings"""
@@ -195,17 +123,17 @@ class carrierEntry(tk.Frame):
 
     def decreaseBlocksize(self):
         """Decrease value of initBlockSize"""
-        blockSizeOld = int(self.initBlocksize.get().strip())
-        blockSizeNew = max(blockSizeOld - 512, 0)
-        self.initBlocksize.delete(0,tk.END)
-        self.initBlocksize.insert(tk.END, str(blockSizeNew))
+        blockSizeOld = int(self.initBlocksize_entry.get().strip())
+        blockSizeNew = max(blockSizeOld - 512, 512)
+        self.initBlocksize_entry.delete(0,tk.END)
+        self.initBlocksize_entry.insert(tk.END, str(blockSizeNew))
 
     def increaseBlocksize(self):
         """Increase value of initBlockSize"""
-        blockSizeOld = int(self.initBlocksize.get().strip())
+        blockSizeOld = int(self.initBlocksize_entry.get().strip())
         blockSizeNew = blockSizeOld + 512
-        self.initBlocksize.delete(0,tk.END)
-        self.initBlocksize.insert(tk.END, str(blockSizeNew))
+        self.initBlocksize_entry.delete(0,tk.END)
+        self.initBlocksize_entry.insert(tk.END, str(blockSizeNew))
 
     def build_gui(self):
         """Build the GUI"""
@@ -218,61 +146,63 @@ class carrierEntry(tk.Frame):
         self.grid_columnconfigure(2, weight=0, pad=3)
         self.grid_columnconfigure(3, weight=0, pad=3)
 
-        tk.Label(self, text='Settings', font=('', 16)).grid(column=0, row=0, sticky='w')
-
         # Entry elements
-
+        ttk.Separator(self, orient='horizontal').grid(column=0, row=0, columnspan=4, sticky='ew')
         # Output Directory
-        tk.Label(self, text='Output Directory').grid(column=0, row=3, sticky='w')
-        self.outDirButton = tk.Button(self, text='Select', command=self.selectOutputDirectory, width=15)
-        self.outDirButton.grid(column=1, row=3, sticky='w')
+        #tk.Label(self, text='Output Directory').grid(column=0, row=3, sticky='w')
+        self.outDirButton_entry = tk.Button(self, text='Select Output Directory', command=self.selectOutputDirectory, width=20)
+        self.outDirButton_entry.grid(column=0, row=3, sticky='w')
         self.outDirLabel = tk.Label(self, text=self.outDir)
         self.outDirLabel.update()
-        self.outDirLabel.grid(column=1, row=4, sticky='w')
-        
+        self.outDirLabel.grid(column=1, row=3, sticky='w')
+
+        ttk.Separator(self, orient='horizontal').grid(column=0, row=5, columnspan=4, sticky='ew')
+
         # Tape Device
-        tk.Label(self, text='Tape Device').grid(column=0, row=5, sticky='w')
-        self.tapeDevice = tk.Entry(self, width=20)
-        self.tapeDevice['background'] = 'white'
-        self.tapeDevice.insert(tk.END,'/dev/nst0')
-        self.tapeDevice.grid(column=1, row=5, sticky='w')
+        tk.Label(self, text='Tape Device').grid(column=0, row=6, sticky='w')
+        self.tapeDevice_entry = tk.Entry(self, width=20)
+        self.tapeDevice_entry['background'] = 'white'
+        self.tapeDevice_entry.insert(tk.END, config.tapeDevice)
+        self.tapeDevice_entry.grid(column=1, row=6, sticky='w')
 
         # Initial Block Size
-        tk.Label(self, text='Initial Block Size').grid(column=0, row=6, sticky='w')
-        self.initBlocksize = tk.Entry(self, width=20)
-        self.initBlocksize['background'] = 'white'
-        self.initBlocksize.insert(tk.END,'512')
-        self.initBlocksize.grid(column=1, row=6, sticky='w')
+        tk.Label(self, text='Initial Block Size').grid(column=0, row=7, sticky='w')
+        self.initBlocksize_entry = tk.Entry(self, width=20)
+        self.initBlocksize_entry['background'] = 'white'
+        self.initBlocksize_entry.insert(tk.END, config.initBlocksize)
+        self.initBlocksize_entry.grid(column=1, row=7, sticky='w')
         self.decreaseBSButton = tk.Button(self, text='-', command=self.decreaseBlocksize, width=1)
-        self.decreaseBSButton.grid(column=2, row=6, sticky='w')
+        self.decreaseBSButton.grid(column=2, row=7, sticky='w')
         self.increaseBSButton = tk.Button(self, text='+', command=self.increaseBlocksize, width=1)
-        self.increaseBSButton.grid(column=3, row=6, sticky='w')
+        self.increaseBSButton.grid(column=3, row=7, sticky='w')
 
         # Sessions
-        tk.Label(self, text='Sessions').grid(column=0, row=7, sticky='w')
-        self.sessions = tk.Entry(self, width=20)
-        self.sessions['background'] = 'white'
-        self.sessions.grid(column=1, row=7, sticky='w')
+        tk.Label(self, text='Sessions (comma-separated list)').grid(column=0, row=8, sticky='w')
+        self.sessions_entry = tk.Entry(self, width=20)
+        self.sessions_entry['background'] = 'white'
+        self.sessions_entry.grid(column=1, row=8, sticky='w')
 
         # Prefix
-        tk.Label(self, text='Prefix').grid(column=0, row=8, sticky='w')
-        self.prefix = tk.Entry(self, width=20)
-        self.prefix['background'] = 'white'
-        self.prefix.insert(tk.END,'session')
-        self.prefix.grid(column=1, row=8, sticky='w')
+        tk.Label(self, text='Prefix').grid(column=0, row=9, sticky='w')
+        self.prefix_entry = tk.Entry(self, width=20)
+        self.prefix_entry['background'] = 'white'
+        self.prefix_entry.insert(tk.END, config.prefix)
+        self.prefix_entry.grid(column=1, row=9, sticky='w')
 
         # Extension
-        tk.Label(self, text='Extension').grid(column=0, row=9, sticky='w')
-        self.extension = tk.Entry(self, width=20)
-        self.extension['background'] = 'white'
-        self.extension.insert(tk.END,'dd')
-        self.extension.grid(column=1, row=9, sticky='w')
+        tk.Label(self, text='Extension').grid(column=0, row=10, sticky='w')
+        self.extension_entry = tk.Entry(self, width=20)
+        self.extension_entry['background'] = 'white'
+        self.extension_entry.insert(tk.END, config.extension)
+        self.extension_entry.grid(column=1, row=10, sticky='w')
 
         # Fill failed blocks
-        tk.Label(self, text='Fill failed blocks').grid(column=0, row=10, sticky='w')
-        fillBlocks = tk.IntVar()
-        self.fillblocks = tk.Checkbutton(self, variable=fillBlocks)
-        self.fillblocks.grid(column=1, row=10, sticky='w')
+        tk.Label(self, text='Fill failed blocks').grid(column=0, row=11, sticky='w')
+        self.fBlocks = tk.IntVar()
+        self.fillblocks_entry = tk.Checkbutton(self, variable=self.fBlocks)
+        self.fillblocks_entry.grid(column=1, row=11, sticky='w')
+
+        ttk.Separator(self, orient='horizontal').grid(column=0, row=12, columnspan=4, sticky='ew')
 
         self.start_button = tk.Button(self,
                                        text='Start',
@@ -280,6 +210,8 @@ class carrierEntry(tk.Frame):
                                        underline=0,
                                        command=self.on_submit)
         self.start_button.grid(column=1, row=13, sticky='w')
+
+        ttk.Separator(self, orient='horizontal').grid(column=0, row=14, columnspan=4, sticky='ew')
 
         # Add ScrolledText widget to display logging info
         st = ScrolledText.ScrolledText(self, state='disabled', height=15)
@@ -300,11 +232,8 @@ class carrierEntry(tk.Frame):
         logger.addHandler(self.text_handler)
 
         # Define bindings for keyboard shortcuts: buttons
-        self.root.bind_all('<Control-Key-n>', self.on_create)
-        self.root.bind_all('<Control-Key-o>', self.on_create)
-        self.root.bind_all('<Control-Key-f>', self.on_create)
+        self.root.bind_all('<Control-Key-s>', self.on_submit)
         self.root.bind_all('<Control-Key-e>', self.on_quit)
-        self.root.bind_all('<Control-Key-s>', self.on_create)
 
         # TODO keyboard shortcuts for Radiobox selections: couldn't find ANY info on how to do this!
 
@@ -369,7 +298,7 @@ def main():
 
     try:
         root = tk.Tk()
-        carrierEntry(root)
+        tapeimgrGUI(root)
 
         t1 = threading.Thread(target=worker.worker, args=[])
         t1.start()
