499f29816c8df0cb5d6f20a90bef0be7b08e0def
bitsgalore <johan.vanderknijff@kb.nl>
2018-12-03 17:44:47 +0100

    bumped version number

--- tapeimgr/tapeimgr.py
+++ tapeimgr/tapeimgr.py
@@ -10,7 +10,7 @@ from .cli import main as cliLaunch
 from .gui import main as guiLaunch
 from . import config
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 def main():
     """Launch GUI if no command line arguments were given; otherwise launch CLI"""
